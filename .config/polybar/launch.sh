b4dd29a3670644d73016998e193ea193de2ef9ee
jacomacena <jacog3@gmail.com>
2026-06-16 19:03:17 -0300

    refactor: update polybar configuration and launch script for improved bar inheritance and process termination

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -17,7 +17,7 @@ export MONITOR="${internal_monitor:-eDP-1}"
 export EXTERNAL_MONITOR="${external_monitor:-HDMI-1}"
 
 # Terminate already running bar instances
-killall -q polybar
+killall -q polybar || true
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
